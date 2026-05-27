620895d7daf7554607bb72eba1ee389019ed10e8
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-28 00:28:56 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class tokenDetchWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = tokenDetchWorker()
+    print("Worker engine initialized.")
